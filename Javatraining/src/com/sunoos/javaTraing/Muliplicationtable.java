574270994c52dbf3a6454953c63b74efed0de952
sunoos <sunoos@macdeMacBook-Air.local>
2019-07-04 01:12:05 +0530

    completed multiplication table

---
+++ Javatraining/src/com/sunoos/javaTraing/Muliplicationtable.java
@@ -0,0 +1,34 @@
+package com.sunoos.javaTraing;
+
+public class Muliplicationtable {
+	static void Table() {
+        System.out.format("     ");
+		for (int i = 1; i <= 9; i++) {
+			System.out.format("%4d",i);
+		}
+		System.out.println();
+		System.out.println("----------------------------------------------");
+		for (int i = 1; i <= 9; i++) {
+			System.out.format("%4d|",i);
+			for(int j=1;j<=9;j++)
+			{
+				System.out.format("%4d",i*j);
+			}
+			System.out.println();
+		}
+	
+	
+		
+		
+	}
+	
+
+	
+	
+	
+	
+	public static void main(String[] args) {
+		Table();
+	}
+
+}
